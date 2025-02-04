403f23f5d029c41b2c7abf2ea40ab06fba7c8e6c
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-02-04 13:29:23 +0530

    Did changes in the js of login

---
+++ login.js
@@ -0,0 +1,22 @@
+function showForm(role) {
+    if (role === "admin") {
+        document.getElementById("admin-form").style.display = "block";
+        document.getElementById("user-form").style.display = "none";
+    } else {
+        document.getElementById("admin-form").style.display = "none";
+        document.getElementById("user-form").style.display = "block";
+    }
+}
+
+let adminsubmit = document.getElementById("admin-submit");
+let usersubmit = document.getElementById("user-submit");
+adminsubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href = "admin.html";
+});
+
+usersubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href = "user.html";
+});
+
