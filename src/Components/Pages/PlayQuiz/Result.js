adb92fccef9156848f640b42a59b629390171ccb
Abhinav Pandey <abhinavpandeydel42@gmail.com>
2025-03-27 22:16:42 +0530

    edits to quizcard and result

--- src/Components/Pages/PlayQuiz/Result.js
+++ src/Components/Pages/PlayQuiz/Result.js
@@ -1,67 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { resetQuiz } from "../../../Redux/Actions/Actions";
+import "./Result.css";
 import img from "../../Images/bg.png";
-import { Button } from "@mui/material";
-function Result(props) {
-  const results = useSelector((state) => state.reducer.answers); //retrieving the array of selected answers
-  const mapped = results.map((el) => el.isCorrect); //array of isCorrect values(either true or false)
 
-  const navigate = useNavigate(); //to navigate to other route
-  const disptach = useDispatch(); //to dispatch action
+const Result = ({ name }) => {
+  const results = useSelector(state => state.reducer.answers);
+  const correctAnswers = results.filter(el => el.isCorrect).length;
+  const totalQuestions = results.length;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  //this func will run when "Done" button is clicked
   const resetQuizHandler = () => {
-
-    disptach(resetQuiz());
+    dispatch(resetQuiz());
     navigate("/");
   };
+
   return (
-    <div>
-      <div
-        className="congo-img"
-        style={{
-          marginTop: "100px",
-          border: "none",
-          borderRadius: "20px",
-          boxShadow: "2px 4px 4px black",
-          height: "70vh",
-          width: "80vw",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
-          alignItems: "center",
-          backgroundImage: `url(${img})`,
-          backgroundSize: "cover",
-          fontFamily: "sans-serif",
-          padding: "10px",
-          textAlign: "center"
-        }}
-      >
-        <div style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around", borderRadius: "10px",
-          alignItems: "center", backgroundColor: "white", padding: "5px", border: "8px solid black"
-        }}>
-          <h2 >Hii ! {props.name} 👋 </h2>
-          <h1>
-            Your Score is {mapped.filter((el) => el === true).length} out of{" "}
-            {mapped.length} {/** the length of element with isCorrect = true will be the number of correct answers out of total */}
-          </h1>
-        </div>
-        <Button
-          variant="contained" size="large"
-          sx={{ backgroundColor: "black", border: "4px solid white", borderRadius: "10px" }}
-          onClick={() => resetQuizHandler()}
-        >
-          Done
-        </Button>
+    <div className="result-container" style={{ backgroundImage: `url(${img})` }}>
+      <div className="result-box">
+        <h2>Hii {name} 👋</h2>
+        <h1>Your Score is {correctAnswers} out of {totalQuestions}</h1>
       </div>
+      <button className="done-button" onClick={resetQuizHandler}>Done</button>
     </div>
   );
-
-}
+};
 export default Result;
