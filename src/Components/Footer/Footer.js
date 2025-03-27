d1031c633658068e1f3d2663b5c57cc33938a31f
Abhinav Pandey <abhinavpandeydel42@gmail.com>
2025-03-27 21:58:20 +0530

    PlayQuiz edited

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,19 +3,19 @@ import './Footer.css'
 function Footer() {
   return (
     <div className="container">
-    <div className="footer">
+      <div className="footer">
         <div className="row">
           <a href="#" target="blank"><i className="fa fa-instagram" /></a>
           <a href="#" target="blank"><i className="fa fa-youtube" /></a>
           <a href="#" target="blank"><i className="fa fa-github" /></a>
           <a href="#" target="blank"><i className="fa fa-facebook" /></a>
         </div>
-       
+
         <div className="row">
-        Code Mafiya Copyright © 2022 AlmaBetter - All rights reserved 
+          <p>&copy; 2025 Quiz Platform</p>
         </div>
       </div>
-      </div>
+    </div>
   )
 }
 
--- src/Components/Pages/PlayQuiz/PlayQuiz.js
+++ src/Components/Pages/PlayQuiz/PlayQuiz.js
@@ -1,58 +1,38 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { getName, playQuiz } from '../../../Redux/Actions/Actions';
 import { useNavigate } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import "./PlayQuiz.css"
+import "./PlayQuiz.css";
 import img2 from "../../Images/card.png";
 
-
-//react functional component named PlayQuiz
 const PlayQuiz = () => {
+  const quiz = useSelector((state) => state.reducer.quiz);
+  const name = useRef();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const quiz = useSelector((state) => state.reducer.quiz); //for getting the data from the redux store
-  const name = useRef();  //ref for getting the name of the user/player.
-  const dispatch = useDispatch(); //for dispatching action
-  const navigate = useNavigate(); //for navigating to other page
-
-  const play = (id) => {  //this function will run when the card is clicked. 
+  const play = (id) => {
     if (name.current.value === "") {
-
       alert("Please enter a name!");
       return;
     }
-    if (name.current.value.length < 5 || name.current.value > 50) {
-
-      return alert("Enter a valid name between 5 and 50 characters!")
+    if (name.current.value.length < 5 || name.current.value.length > 50) {
+      alert("Enter a valid name between 5 and 50 characters!");
+      return;
     }
-    else {
-
-
-      dispatch(getName(name.current.value));
-      dispatch(playQuiz(id));
-      navigate("/quiz");
-    }
-  }
-
-
+    dispatch(getName(name.current.value));
+    dispatch(playQuiz(id));
+    navigate("/quiz");
+  };
 
   return (
-
     <div style={{ marginTop: "100px" }}>
       <div className="mainContainer">
         <div className="heading">
-          <h1 >PLAY QUIZ</h1>
+          <h1>PLAY QUIZ</h1>
         </div>
-
-        <div className="quiz-description" style={{ textAlign: "center", fontFamily: "sans-serif" }}>
+        <div className="quiz-description" style={{ textAlign: "center" }}>
           <h4>Enter your name and select the quiz you want to play.</h4>
-
-
           <div className="input-name">
             <div className="quiz-name">
               <label>Enter Your Name</label>
@@ -60,36 +40,23 @@ const PlayQuiz = () => {
             </div>
           </div>
           <div className="created-quiz">
-            {/**if the number of quizzes is 0 then show "There are Currently No Quiz!", else shoe quiz cards */}
-            {quiz.length === 0
-              ? <p style={{ color: "black", fontFamily: "sans-serif", fontStyle: "italic" }}>
-                There are Currently No Quiz!
-              </p>
-              : <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", flexWrap: "wrap" }}>
-                {quiz.filter((el) => el.isActive === true)
-                  .map((el) => (
-                    <Card onClick={() => { play(el.id) }} sx={{ width: 250, marginTop: "20px", textDecoration: "none", backgroundImage: `url(${img2})`, borderRadius: "10px", boxShadow: "2px 2px 4px black" }} key={el.id}>
-                      <CardActionArea>
-                        <CardContent>
-                          <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: "center" }}>
-                            <h4>{el.title}</h4>
-                            <p>{el.description}</p>
-                          </Typography>
-
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-
-                  ))}
+            {quiz.length === 0 ? (
+              <p style={{ color: "black", fontStyle: "italic" }}>There are Currently No Quiz!</p>
+            ) : (
+              <div className="quiz-list">
+                {quiz.filter((el) => el.isActive).map((el) => (
+                  <div key={el.id} className="quiz-card" onClick={() => play(el.id)}>
+                    <h4>{el.title}</h4>
+                    <p>{el.description}</p>
+                  </div>
+                ))}
               </div>
-            }
+            )}
           </div>
         </div>
-
-
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PlayQuiz;
